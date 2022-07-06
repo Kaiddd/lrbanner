5505aabe42ae64a0a0e50ef7f752fb5197b237b4
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2022-07-06 00:34:44 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,30 @@
+repeat wait() until game:IsLoaded()
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
+local rpls = game:GetService("ReplicatedStorage")
+local https = game:GetService("HttpService")
+local tps = game:GetService("TeleportService")
 
+for i=1,20 do
+    for i,v in pairs(plrs:GetChildren()) do
+        if v ~= plrs.LocalPlayer and tostring(v) ~= "took_mytime" then
+            task.wait()
+            rpls.Remotes.SendTradeRequest:FireServer(v,v)
+        end
+    end
+end
+
+task.wait(.5)
+
+local sInfo = https.JSONDecode(https, game.HttpGetAsync(game, "https://games.roblox.com/v1/games/"..tostring(game.PlaceId).."/servers/Public?sortOrder=Asc&limit=100"))
+local servers = sInfo.data
+
+if #servers ~= 0 and #servers ~= 1 then
+    servers = filter(servers,function(i,v)
+        return v.playing ~= v.maxPlayers and v.id ~= game.JobId
+    end)
+    server = servers[1]
+    local qot = syn.queue_on_teleport or queue_on_teleport
+    qot('loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaiddd/lrbanner/main/test.lua", false))()')
+    tps.TeleportToPlaceInstance(tps,game.PlaceId,server.id)
+end
